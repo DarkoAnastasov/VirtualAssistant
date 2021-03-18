6e9c0990594d6fd24e9b1042a6fc381d62093002
Darko Anastasov <anastasovdarko95@gmail.com>
2021-03-18 23:52:31 +0100

    updated

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ def talk(text):
 
     engine.say(text)
     engine.runAndWait()
+def take_command():
+
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'siri' in command:
+                command=command.replace('siri', '')
+                print(command)
+    except:
+        pass
+    return command
+
